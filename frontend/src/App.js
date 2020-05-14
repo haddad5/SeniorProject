b58d0049eb8162fb9d37943299571b9d3a18877b
haddad5 <31868827+haddad5@users.noreply.github.com>
2020-05-14 14:15:17 -0400

    menus are updated to remember what was entered.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,6 +60,7 @@ export default class App extends React.Component {
     this.state = {
       loading: true,
       tableData: null,
+      
     };
   }
   componentDidMount() {
--- frontend/src/MyMenu.js
+++ frontend/src/MyMenu.js
@@ -1,39 +1,58 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
+import Chip from '@material-ui/core/Chip';
 import MenuItem from '@material-ui/core/MenuItem';
 import {ThemeProvider} from '@material-ui/core/styles';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
 
-export default function SimpleMenu(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const createMenu = () => {
-    const keys = props.items;
-    return keys.map((key, index)=>{
-      return <MenuItem key={key}>{key}</MenuItem>;
-    });
-  };
-
-  return (
-    <ThemeProvider theme={props.newTheme}>
-      <Button aria-controls="simple-menu"
-        aria-haspopup="true" onClick={handleClick}
-        variant="contained" color="primary" size="large">
-        {props.name}<ArrowDropDownIcon/>
-      </Button>
-      <Menu anchorEl={anchorEl} keepMounted
-        open={Boolean(anchorEl)} onClose={handleClose}>
-        {createMenu()}
-      </Menu>
-    </ThemeProvider>
-  );
+export default class SimpleMenu extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      choices: [],
+    };
+  }
+    
+  render() {
+    const props = this.props;
+    const choices = this.state.choices;
+    
+    const handleChange = (event) => {
+        this.setState({choices: event.target.value,});
+    };
+    
+    return (
+      <FormControl style={{minWidth: 120, maxWidth: 300}}>
+        <InputLabel>{props.name}</InputLabel>
+          <Select
+            multiple
+            value={choices}
+            onChange={handleChange}
+            input={<Input/>}
+            renderValue={selected => (
+              <div>
+                {selected.map(value => (
+                  <Chip key={value} label={value} />
+                ))}
+              </div>
+            )}
+          >
+            {props.items.map(item => (
+              <MenuItem
+                key={item}
+                value={item}
+              >
+                {item}
+              </MenuItem>        
+            ))}
+          </Select>
+      </FormControl>
+    );
+  }
 }
